6022b5d45eb49658bbe73fa7109022fc430ed83c
kyell <kyell@kraftstories.com>
2026-03-04 11:15:42 +0800

    Added newer resume

--- components/about.tsx
+++ components/about.tsx
@@ -123,7 +123,7 @@ export function About() {
               </div>
 
               <Button className="w-full mt-8 bg-transparent" variant="outline" asChild>
-                <a href="/Dimatatac_Resume.pdf" download>
+                <a href="/Kyell_Dimatatac_Resume.pdf" download>
                   <Download className="mr-2 h-4 w-4" />
                   Download Resume
                 </a>
